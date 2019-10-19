52c942b8c1348950003f64f808355c9b11c588c2
Albert Hsu <iverson52000@gmail.com>
2019-10-19 11:16:50 -0700

    20191019

---
+++ 380. Insert Delete GetRandom O(1).py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 10:28:38 2019
+
+@author: alberthsu
+"""
+
+"""
+380. Insert Delete GetRandom O(1)
+Design a data structure that supports all following operations in average O(1) time.
+1.	insert(val): Inserts an item val to the set if not already present.
+2.	remove(val): Removes an item val from the set if present.
+3.	getRandom: Returns a random element from current set of elements. Each element must have the same probability of being returned.
+"""
+
+#array+hashmap
+
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.nums = []
+        self.m = {}
+        
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.m: return False
+        self.nums.append(val)
+        self.m[val] = len(self.nums)-1
+        return True
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.m: return False
+        last_val = self.nums[-1]
+        self.nums[self.m[val]], self.nums[-1] = self.nums[-1], self.nums[self.m[val]]
+        self.m[last_val] = self.m[val]
+        self.nums.pop()
+        self.m.pop(val)
+        return True
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.nums[random.randint(0, len(self.nums) - 1)]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
---
+++ 399. Evaluate Division.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 19 11:08:39 2019
+
+@author: alberthsu
+"""
+
+"""
+ !399. Evaluate Division
+Equations are given in the format A / B = k, where A and B are variables represented as strings, and k is a real number (floating point number). Given some queries, return the answers. If the answer does not exist, return -1.0.
+Example:
+Given a / b = 2.0, b / c = 3.0.
+queries are: a / c = ?, b / a = ?, a / e = ?, a / a = ?, x / x = ? .
+return [6.0, 0.5, -1.0, 1.0, -1.0 ].
+"""
+
+#graph+bfs
+
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        res = []
+        g = collections.defaultdict(tuple)
+        self.buildGraph(g, equations, values)        
+        for query in queries: self.bfs(res, g, query)
+        return res
+        
+    def buildGraph(self, g, equations, values):
+        def addEdge(nom, denom, value):
+            if nom in g: g[nom].append((denom, value))
+            else: g[nom] = [(denom, value)]
+                
+        for vertices, value in zip(equations, values):
+            nom, denom = vertices
+            addEdge(nom, denom, value)
+            addEdge(denom, nom, 1/value)                      
+        
+    def bfs(self, res, g, query):
+        nom, denom = query
+        if nom not in g or denom not in g: 
+            res.append(-1.0)
+            return
+        q = collections.deque([(nom, 1.0)])
+        visited = set()
+        while q:
+            pop_nom, pop_value = q.popleft()
+            if pop_nom == denom: 
+                res.append(pop_value)
+                return
+            visited.add(nom)
+            for denom_g, value in g[nom_pop]:
+                if denom_g not in visited:
+                    q.append((denom_g, pop_value*value))
+        res.append(-1.0)
