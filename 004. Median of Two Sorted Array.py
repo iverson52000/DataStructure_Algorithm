ac4773cd4adb2f0afe16dae2da6dbf3fdb718201
Albert Hsu <iverson52000@gmail.com>
2019-10-30 13:26:56 -0700

    20191030

--- 004. Median of Two Sorted Array.py
+++ 004. Median of Two Sorted Array.py
@@ -25,16 +25,17 @@ class Solution:
             mid1 = (left+right)//2
             mid2 = (n1+n2+1)//2-mid1
             
-            max_left_1 = nums1[mid1-1] if mid1 > 0 else float('-inf')
-            max_left_2 = nums2[mid2-1] if mid2 > 0 else float('-inf')
-            min_right_1 = nums1[mid1] if mid1 < n1 else float('inf')
-            min_right_2 = nums2[mid2] if mid2 < n2 else float('inf')
+            left_max_1 = nums1[mid1-1] if mid1 > 0 else float('-inf')
+            left_max_2 = nums2[mid2-1] if mid2 > 0 else float('-inf')
+            right_min_1 = nums1[mid1] if mid1 < n1 else float('inf')
+            right_min_2 = nums2[mid2] if mid2 < n2 else float('inf')
             
-            if max_left_1 <= min_right_2 and max_left_2 <= min_right_1:
+            if left_max_1 <= right_min_2 and left_max_2 <= right_min_1:
                 if (n1+n2)%2 == 0:
-                    return (max(max_left_1, max_left_2)+min(min_right_1, min_right_2))/2
+                    return (max(left_max_1, left_max_2)+min(right_min_1, right_min_2))/2
                 else:
-                    return max(max_left_1, max_left_2)
-            elif max_left_1 > min_right_2: right = mid1-1
+                    return max(left_max_1, left_max_2)
+            elif left_max_1 > right_min_2: right = mid1-1
             else: left = mid1+1                
+             
 
