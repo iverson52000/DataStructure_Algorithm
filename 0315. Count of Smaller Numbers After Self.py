bb858bc79d2d2e190b5acc4904c48cb9bbfdfdf3
Albert Hsu <iverson52000@gmail.com>
2019-11-09 10:31:57 -0800

    20191109

--- 0315. Count of Smaller Numbers After Self.py
+++ 0315. Count of Smaller Numbers After Self.py
@@ -12,6 +12,8 @@ You are given an integer array nums and you have to return a new counts array.
 The counts array has the property where counts[i] is the number of smaller elements to the right of nums[i].
 """
 
+#binary search
+
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
         if not nums: return None
---
+++ 0359. Logger Rate Limiter.py
@@ -0,0 +1,20 @@
+"""
+$359. Logger Rate Limiter
+Design a logger system that receive stream of messages along with its timestamps, each message should 
+be printed if and only if it is not printed in the last 10 seconds.
+Given a message and a timestamp (in seconds granularity), return true if the message should be printed 
+n the given timestamp, otherwise returns false.
+It is possible that several messages arrive roughly at the same time.
+"""
+
+#hashmap
+
+m = {}
+if message not in m:
+    m[message] = timestamp
+    return True
+if timestamp-m[message] >= 10:
+    m[message] = timestamp
+    return True
+return False
+
