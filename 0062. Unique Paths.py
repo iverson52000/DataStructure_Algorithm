a6e485e53acd4e5a00149cf916ff20393195cc09
Albert Hsu <iverson52000@gmail.com>
2019-11-21 15:25:56 -0800

    20191121

---
+++ 0062. Unique Paths.py
@@ -0,0 +1,35 @@
+"""
+62. Unique Paths
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+How many possible unique paths are there?
+"""
+
+#dp
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1]*n for i in range(m)]
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c]+dp[r][c-1]
+        return dp[m-1][n-1]
+#1d dp
+
+class Solution:
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [1]*n
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[c] += dp[c-1]
+        return dp[n-1]
---
+++ 0063. Unique Paths II.py
@@ -0,0 +1,47 @@
+"""
+63. Unique Paths II
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+"""
+
+#dp
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        if not obstacleGrid or not obstacleGrid[0] or obstacleGrid[0][0] == 1: return 0
+        
+        rlen = len(obstacleGrid); clen = len(obstacleGrid[0])
+        dp = [[0 for i in range(clen+1)] for j in range(rlen+1)]
+        dp[0][1] = 1
+        for r in range(1, rlen+1):
+            for c in range(1, clen+1):
+                if obstacleGrid[r-1][c-1] == 1: continue
+                dp[r][c] = dp[r-1][c]+dp[r][c-1]
+        return dp[rlen][clen]
+
+#1d dp
+
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        """
+        :type obstacleGrid: List[List[int]]
+        :rtype: int
+        """
+        if not obstacleGrid or not obstacleGrid[0] or obstacleGrid[0][0] == 1: return 0
+        
+        n_r = len(obstacleGrid); n_c = len(obstacleGrid[0])
+        dp = [0 for i in range(n_c+1)]
+        dp[1]= 1
+        for r in range(1, n_r+1):
+            for c in range(1, n_c+1):
+                if obstacleGrid[r-1][c-1] == 1: 
+                    dp[c] = 0
+                    continue
+                dp[c] += dp[c-1]
+        return dp[n_c]
+
