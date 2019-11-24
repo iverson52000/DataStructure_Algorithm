e0498c12c76572bf0019ff2f9bd3369b8e9f41db
Albert Hsu <iverson52000@gmail.com>
2019-11-24 09:58:14 -0800

    20191124

---
+++ 0079. Word Search.py
@@ -0,0 +1,35 @@
+"""
+79. Word Search
+Given a 2D board and a word, find if the word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells 
+are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+"""
+
+#dfs
+
+class Solution:
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        if not board or not board[0]: return False
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if self.dfs(board, word, r, c):
+                    return True
+        return False   
+        
+    def dfs(self, board, word, r, c):
+        if len(word) == 0: return True
+        if r >= len(board) or r < 0 or c >= len(board[0]) or c < 0 or board[r][c] != word[0]: 
+            return False
+        tmp = board[r][c]
+        board[r][c] = '#'
+        res = self.dfs(board, word[1:], r+1, c) or\
+        self.dfs(board, word[1:], r-1, c) or\
+        self.dfs(board, word[1:], r, c+1) or\
+        self.dfs(board, word[1:], r, c-1)
+        board[r][c] = tmp
+        return res
--- 0844. Backspace String Compare.py
+++ 0844. Backspace String Compare.py
@@ -1,6 +1,7 @@
 """
 844. Backspace String Compare
-Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+Given two strings S and T, return if they are equal when both are typed into empty text editors. 
+# means a backspace character.
 """
 
 #stack
