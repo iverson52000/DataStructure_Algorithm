92737a0ade495df2f19c80213b4a6298f96803a7
Albert Hsu <iverson52000@gmail.com>
2019-12-14 10:55:37 -0800

    20191214

---
+++ 0403. Frog Jump.py
@@ -0,0 +1,30 @@
+"""
+403. Frog Jump
+A frog is crossing a river. The river is divided into x units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+Given a list of stones' positions (in units) in sorted ascending order, determine if the frog is able to cross the river by landing on the last stone. Initially, the frog is on the first stone and assume the first jump must be 1 unit.
+If the frog's last jump was k units, then its next jump must be either k - 1, k, or k + 1 units. Note that the frog can only jump in the forward direction.
+Note:
+•   The number of stones is ≥ 2 and is < 1,100.
+•   Each stone's position will be a non-negative integer < 231.
+•   The first stone's position is always 0.
+"""
+
+#dfs
+
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        memo = set()
+        target = stones[-1]
+        stones = set(stones)
+        return self.dfs(stones, memo, 1, 1, target)
+ 
+    def dfs(self, stones, memo, cur, k, target) -> bool:
+        if cur == target: return True         
+        if cur > target or cur < 0 or k <= 0 or cur not in stones: return False  
+        if (cur, k) in memo: return False
+        units = [k-1, k, k+1]
+        for unit in units:
+            if (cur+unit) in stones:
+                if self.dfs(stones, memo, cur+unit, unit, target): return True
+        memo.add((cur, k))
+        return False
--- 0450. Delete Node in a BST.py
+++ 0450. Delete Node in a BST.py
@@ -18,7 +18,7 @@ Note: Time complexity should be O(height of tree).
 
 class Solution:
     def deleteNode(self, root: TreeNode, key: int) -> TreeNode:
-        if not root: return root
+        if not root: return None
         if root.val > key: root.left = self.deleteNode(root.left, key)
         elif root.val < key: root.right= self.deleteNode(root.right, key)
         else: 
