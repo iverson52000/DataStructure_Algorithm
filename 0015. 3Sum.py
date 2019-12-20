97f684fbb575924047c0122da4f0ce0105c24154
Albert Hsu <iverson52000@gmail.com>
2019-12-20 10:26:34 -0800

    20191220

--- 0015. 3Sum.py
+++ 0015. 3Sum.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct  4 11:30:00 2019
-
-@author: alberthsu
-"""
-
 """
 15. 3Sum
 Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0 
@@ -18,21 +10,19 @@ The solution set must not contain duplicate triplets.
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
-        if not nums or nums[0] > 0 or nums[-1] < 0: return []
+        if not nums: return []
         res = []
-        for i in range(len(nums)-2):
-            if nums[i] > 0: break
+        n = len(nums)
+        nums.sort()
+        for i in range(n-2):
             if i > 0 and nums[i] == nums[i-1]: continue
-            left = i+1; right = len(nums)-1
+            left = i+1; right = n-1
             while left < right:
                 summ = nums[i]+nums[left]+nums[right]
                 if summ == 0:
                     res.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left+1]: left += 1
-                    while left < right and nums[right] == nums[right-1]: right -= 1
                     left += 1; right -= 1
-                elif summ < 0: left += 1
-                else: right -= 1
-        return res 
+                elif summ > 0: right -= 1
+                else: left += 1
+        return  list(set(map(tuple, res)))
 
---
+++ 0739. Daily Temperatures.py
@@ -0,0 +1,18 @@
+"""
+739. Daily Temperatures
+Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+"""
+
+#stack
+
+class Solution:
+    def dailyTemperatures(self, T: List[int]) -> List[int]:
+        res = [0]*len(T)
+        stack = []
+        for i, t in enumerate(T):
+          while stack and t > T[stack[-1]]:
+            i_pop = stack.pop()
+            res[i_pop] = i-i_pop
+          stack.append(i)
+        return res
