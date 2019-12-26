96a401a30b19cc9ed83924b95595b5e685120276
Albert Hsu <iverson52000@gmail.com>
2019-12-26 16:08:25 +0800

    20191226

--- 0084. Largest Rectangle in Histogram.py
+++ 0084. Largest Rectangle in Histogram.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct 17 10:58:28 2019
-
-@author: alberthsu
-"""
-
 """
 !84. Largest Rectangle in Histogram
 Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
