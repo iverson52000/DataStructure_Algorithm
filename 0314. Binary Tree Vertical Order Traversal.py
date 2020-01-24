be997c830c8e6626c81107767e16864c0e6aabdc
Albert Hsu <iverson52000@gmail.com>
2020-01-24 09:32:16 -0800

    20200124

--- 0314. Binary Tree Vertical Order Traversal.py
+++ 0314. Binary Tree Vertical Order Traversal.py
@@ -1,10 +1,11 @@
 """
 $314. Binary Tree Vertical Order Traversal
-Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to 
+bottom, column by column).
 If two nodes are in the same row and column, the order should be from left to right.
 """
 
-#dfs
+#bfs+hashmap
 
 import collections
 res = []
