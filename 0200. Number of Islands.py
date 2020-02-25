c86727e81d406bc284856ebefcc638f37f492fa1
Albert Hsu <iverson52000@gmail.com>
2020-02-25 09:34:51 -0800

    20200225

--- 0200. Number of Islands.py
+++ 0200. Number of Islands.py
@@ -21,10 +21,9 @@ class Solution:
     def dfs(self, grid, r, c):
         if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] != '1': return
         grid[r][c] = '0'
-        self.dfs(grid, r+1, c)
-        self.dfs(grid, r-1, c)
-        self.dfs(grid, r, c+1)
-        self.dfs(grid, r, c-1)
+        for dir0, dir1 in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+            self.dfs(grid, r+dir0, c+dir1)
+
 
 
 
