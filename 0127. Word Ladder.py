3f12b214f9898b31555523f020d3cf5bb8c0f75f
Albert Hsu <iverson52000@gmail.com>
2020-02-27 10:07:19 -0800

    20200227

--- 0127. Word Ladder.py
+++ 0127. Word Ladder.py
@@ -5,11 +5,13 @@ Given two words (beginWord and endWord), and a dictionary's word list, find the 
 2.	Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
 """
 
-#2/26/2020
+# 2/26/2020
 
-#bfs
+# bfs
+
 
 class Solution:
+
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordList = set(wordList)
         q = collections.deque()
@@ -18,11 +20,12 @@ class Solution:
         chars = string.ascii_lowercase
         while q:
             word, level = q.popleft()
-            if word == endWord: return level
+            if word == endWord:
+                return level
             for i in range(len(word)):
                 for char in chars:
-                    next_word = word[:i]+char+word[i+1:]
+                    next_word = word[:i] + char + word[i + 1:]
                     if next_word in wordList and next_word not in visited:
-                        q.append((next_word, level+1))
+                        q.append((next_word, level + 1))
                         visited.add(next_word)
         return 0
--- 0301. Remove Invalid Parentheses.py
+++ 0301. Remove Invalid Parentheses.py
@@ -1,43 +1,43 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct  7 10:14:28 2019
-
-@author: alberthsu
-"""
 
 """
 301. Remove Invalid Parentheses
 Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
 """
 
-#bfs
+# bfs
+
+
 class Solution:
+
     def removeInvalidParentheses(self, s: str) -> List[str]:
         res = []
         q = collections.deque()
         q.append(s)
         visited = set()
         found = False
-        
         while q:
-            cur = q.popleft()
-            if self.isValid(cur):
-                res.append(cur)
+            s_pop = q.popleft()
+            if self.isValid(s_pop):
+                res.append(s_pop)
                 found = True
-            if found: continue
-            for i in range(len(cur)):
-                if cur[i] not in ['(', ')']: continue
-                next_s = cur[:i]+cur[i+1:]
-                if next_s not in visited:
-                    q.append(next_s)
-                    visited.add(next_s)
+            if found:
+                continue
+            for i in range(len(s_pop)):
+                if s_pop[i] not in ('(', ')'):
+                    continue
+                s_next = s_pop[:i] + s_pop[i + 1:]
+                if s_next not in visited:
+                    q.append(s_next)
+                    visited.add(s_next)
         return res
-                
-    def isValid(self, cur):
+
+    def isValid(self, s) -> bool:
         left = 0
-        for i in range(len(cur)):
-            if cur[i] == '(': left += 1
-            if cur[i] == ')': left -= 1
-            if left < 0: return False
+        for c in s:
+            if c == '(':
+                left += 1
+            elif c == ')':
+                left -= 1
+            if left < 0:
+                return False
         return left == 0
