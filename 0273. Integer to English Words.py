bfc5229e7b8b94bd130c8a5f31e4d05b684cea35
Albert Hsu <iverson52000@gmail.com>
2020-03-01 10:40:20 -0800

    20200301

---
+++ 0273. Integer to English Words.py
@@ -0,0 +1,36 @@
+"""
+!273. Integer to English Words
+Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 2^31 - 1.
+"""
+
+# dfs
+
+
+class Solution:
+
+    def __init__(self):
+        self.lessThan20 = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                           "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
+        self.tens = ["", "Ten", "Twenty", "Thirty", "Forty",
+                     "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
+        self.thousands = ["", "Thousand", "Million", "Billion"]
+
+    def numberToWords(self, num: int) -> str:
+        if num == 0:
+            return "Zero"
+        res = ""
+        for i in range(len(self.thousands)):
+            if num % 1000 != 0:
+                res = self.helper(num % 1000) + self.thousands[i] + " " + res
+            num //= 1000
+        return res.strip()
+
+    def helper(self, num) -> str:
+        if num == 0:
+            return ""
+        elif num < 20:
+            return self.lessThan20[num] + " "
+        elif num < 100:
+            return self.tens[num // 10] + " " + self.helper(num % 10)
+        else:
+            return self.lessThan20[num // 100] + " Hundred " + self.helper(num % 100)
