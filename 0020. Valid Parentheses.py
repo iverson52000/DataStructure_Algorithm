3f5bc9a9ab6e0970e27230fceb65bd4f47717c3b
Albert Hsu <iverson52000@gmail.com>
2020-03-02 09:22:38 -0800

    20200302

--- 0020. Valid Parentheses.py
+++ 0020. Valid Parentheses.py
@@ -5,17 +5,25 @@ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', d
 the input string is valid.
 """
 
-#stack
+# stack
+
 
 class Solution:
+
     def isValid(self, s: str) -> bool:
-        if not s: return True
+        if not s:
+            return True
         stack = []
         for char in s:
-            if char in ('(', '[', '{'): stack.append(char)
+            if char in ('(', '[', '{'):
+                stack.append(char)
             else:
-                if not stack: return False
-                elif char == ')' and stack.pop() != '(': return False
-                elif char == ']' and stack.pop() != '[': return False
-                elif char == '}' and stack.pop() != '{': return False
-        return len(stack) == 0 
+                if not stack:
+                    return False
+                elif char == ')' and stack.pop() != '(':
+                    return False
+                elif char == ']' and stack.pop() != '[':
+                    return False
+                elif char == '}' and stack.pop() != '{':
+                    return False
+        return len(stack) == 0
---
+++ 0067. Add Binary.py
@@ -0,0 +1,22 @@
+"""
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+"""
+
+# recursion
+
+
+class Solution:
+
+    def addBinary(self, a: str, b: str) -> str:
+        if len(a) == 0:
+            return b
+        if len(b) == 0:
+            return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[:-1], b[:-1]), '1') + '0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[:-1], b[:-1]) + '0'
+        else:
+            return self.addBinary(a[:-1], b[:-1]) + '1'
