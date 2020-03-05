5630498898ef7c486f5fbc8065ccf30ef9305fd7
Albert Hsu <iverson52000@gmail.com>
2020-03-05 09:47:32 -0800

    20200305

--- 0076. Minimum Window Substring.py
+++ 0076. Minimum Window Substring.py
@@ -4,15 +4,18 @@ Given a string S and a string T, find the minimum window in S which will contain
 in T in complexity O(n).
 """
 
-#Two pointer+hashmap
+# Two pointer+hashmap
+
 
 class Solution:
+
     def minWindow(self, s: str, t: str) -> str:
         missing = len(t)
         m = collections.Counter(t)
-        left = 0; left_min = right_min = 0
+        left = left_min = right_min = 0
         for i in range(len(s)):
-            if m[s[i]] > 0: missing -= 1
+            if m[s[i]] > 0:
+                missing -= 1
             m[s[i]] -= 1
             if missing == 0:
                 while left < i and m[s[left]] < 0:
@@ -20,8 +23,7 @@ class Solution:
                     left += 1
                 m[s[left]] += 1
                 missing += 1
-                if right_min == 0 or i+1-left < right_min-left_min:
-                    right_min, left_min = i+1, left
+                if right_min == 0 or i + 1 - left < right_min - left_min:
+                    right_min, left_min = i + 1, left
                 left += 1
         return s[left_min:right_min]
-
