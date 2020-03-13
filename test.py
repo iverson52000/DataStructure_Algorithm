d9b76e195d70e46813a628b5a8162b1debe6864a
Albert Hsu <iverson52000@gmail.com>
2020-03-13 09:27:25 -0700

    20200313

--- test.py
+++ test.py
@@ -1,26 +1,9 @@
+def fib(N):
+    if N == 0: return 0
+    memo = [0,1]
+    for _ in range(2,N+1):
+        memo = [memo[-1], memo[-1] + memo[-2]]
 
+    return memo[-1]
 
-m = collections.defaultdict(int)
-for transaction in transactions:
-    m[transaction[0]] += transaction[2]
-    m[transaction[1]] -= transaction[2]
-
-balances = []
-for val in m.values():
-    if val != 0: balances.append(val)
-
-res = dfs(0, balances)
-
-def dfs(level, balances) -> int:
-    if level == len(balances): return 0
-    cur_num = balances[level]
-    if cur_num == 0: return dfs(level+1, balances)
-    minn = flaot('inf')
-    for i in range(level+1, len(balances)):
-        nxt = balances[i]
-        if cur*nxt < 0:
-            balances[i] = cur+nxt
-            minn = min(minn, 1+dfs(i))
-
-
-#test test
+    #test
