70dbaf9e4b2ff4e7d3b3d32ea7defa0287bb97e1
Albert Hsu <iverson52000@gmail.com>
2020-03-17 09:37:18 -0700

    20200317

--- 0560. Subarray Sum Equals K.py
+++ 0560. Subarray Sum Equals K.py
@@ -8,11 +8,11 @@ subarrays whose sum equals to k.
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        if not nums: return 0
+        res = 0
+        summ = 0
         m = {0: 1}
-        res = 0; summ = 0
-        for i in range(len(nums)):
-            summ += nums[i]
+        for num in nums:
+            summ += num
             res += m.get(summ-k, 0)
             m[summ] = m.get(summ, 0)+1
         return res
