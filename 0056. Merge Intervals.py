872ab7612e0a81838a88dc16b6a071081f7d2b57
Albert Hsu <iverson52000@gmail.com>
2020-03-23 08:53:21 -0700

    20200323

--- 0056. Merge Intervals.py
+++ 0056. Merge Intervals.py
@@ -3,14 +3,17 @@
 Given a collection of intervals, merge all overlapping intervals.
 """
 
+
 class Solution:
+
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals: return []
-        intervals.sort(key = lambda x: x[0])	#nedd to sort first!
-        res = [intervals[0]]	#list of list
+        if not intervals:
+            return []
+        intervals.sort(key=lambda x: x[0])  # need to sort first!
+        res = [intervals[0]]  # list of list
         for interval in intervals[1:]:
             if interval[0] <= res[-1][1]:
                 res[-1][1] = max(res[-1][1], interval[1])
-            else: res.append(interval)
-        return res 
-
+            else:
+                res.append(interval)
+        return res
--- 0124. Binary Tree Maximum Path Sum.py
+++ 0124. Binary Tree Maximum Path Sum.py
@@ -4,7 +4,7 @@ Given a non-empty binary tree, find the maximum path sum.
 For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
 """
 
-#dfs
+# dfs
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,16 +13,20 @@ For this problem, a path is defined as any sequence of nodes from some starting 
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
+
     def maxPathSum(self, root: TreeNode) -> int:
-        if not root: return 0
+        if not root:
+            return 0
         self.res = float('-inf')
-        self.dfs(root, self.res)
+        self.dfs(root)
         return self.res
-    
-    def dfs(self, root, res) -> int:
-        if not root: return 0
-        left_val = max(self.dfs(root.left, self.res), 0)
-        right_val = max(self.dfs(root.right, self.res), 0)
-        self.res = max(self.res, root.val+left_val +right_val)
-        return max(left_val, right_val)+root.val
+
+    def dfs(self, root) -> int:
+        if not root:
+            return 0
+        left_val = max(self.dfs(root.left), 0)
+        right_val = max(self.dfs(root.right), 0)
+        self.res = max(self.res, root.val + left_val + right_val)
+        return max(left_val, right_val) + root.val
