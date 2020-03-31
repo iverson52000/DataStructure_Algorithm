1eb4ea0fe3aa846a33f448846d90341ffd845c15
Albert Hsu <iverson52000@gmail.com>
2020-03-31 09:10:50 -0700

    20200331

--- 0257. Binary Tree Paths.py
+++ 0257. Binary Tree Paths.py
@@ -3,7 +3,7 @@
 Given a binary tree, return all root-to-leaf paths.
 """
 
-#preorder dfs
+# preorder dfs
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -12,34 +12,40 @@ Given a binary tree, return all root-to-leaf paths.
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
+
     def binaryTreePaths(self, root: TreeNode) -> List[str]:
-        if not root: return []
+        if not root:
+            return []
         path = ''
         res = []
         self.dfs(root, path, res)
         return res
-    
+
     def dfs(self, root, path, res):
-        if not root: return
-        path = path+str(root.val)
+        if not root:
+            return
+        path = path + str(root.val)
         if not root.left and not root.right:
             res.append(path)
             return
-        self.dfs(root.left, path+'->', res)
-        self.dfs(root.right, path+'->', res)
+        self.dfs(root.left, path + '->', res)
+        self.dfs(root.right, path + '->', res)
 
-#Iteration
+# Iteration
 
-def binaryTreePaths1(self, root):
-    if not root: return []
-    res, stack = [], [(root, "")]
-    while stack:
-        node, path = stack.pop()
-        if not node.left and not node.right:
-            res.append(path +str(node.val))
-        if node.right:
-            stack.append((node.right, path +str(node.val)+"->"))
-        if node.left:
-            stack.append((node.left, path +str(node.val)+"->"))
-    return res
+class Solution:
+    def binaryTreePaths1(self, root):
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                res.append(path + str(node.val))
+            if node.right:
+                stack.append((node.right, path + str(node.val) + "->"))
+            if node.left:
+                stack.append((node.left, path + str(node.val) + "->"))
+        return res
