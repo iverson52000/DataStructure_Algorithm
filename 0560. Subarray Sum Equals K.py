cf95bda9b75795f27b7aa1361837aec71e687636
Albert Hsu <iverson52000@gmail.com>
2020-04-22 09:11:18 -0700

    20200422

--- 0560. Subarray Sum Equals K.py
+++ 0560. Subarray Sum Equals K.py
@@ -4,15 +4,18 @@ Given an array of integers and an integer k, you need to find the total number o
 subarrays whose sum equals to k.
 """
 
-#prefix sum+hashmap
+# 4/22/2020
+# prefix sum+hashmap
+
 
 class Solution:
+
     def subarraySum(self, nums: List[int], k: int) -> int:
         res = 0
         summ = 0
         m = {0: 1}
         for num in nums:
             summ += num
-            res += m.get(summ-k, 0)
-            m[summ] = m.get(summ, 0)+1
+            res += m.get(summ - k, 0)
+            m[summ] = m.get(summ, 0) + 1
         return res
