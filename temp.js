b4aa998b6a16ef5761d5c9a2590964801f690c7f
Albert Hsu <iverson52000@gmail.com>
2020-04-28 10:11:39 -0700

    20200428

--- temp.js
+++ temp.js
@@ -4,7 +4,4 @@ const multiplyBy = (num1) => (num2) => num1*num2;
 const multiplyByTwo = multiplyBy(2);
 const multiplyByFive = multiplyBy(5);
 
-multiplyByTwo(2);
-multiplyByTwo(5);
-multiplyByFive(10);
 multiplyByFive(20);
