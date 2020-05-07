713bb75812fce85ef01a5f383a34f5155049d611
Albert Hsu <iverson52000@gmail.com>
2020-05-07 09:30:28 -0700

    20200507

--- 0138. Copy List With Random Pointer.py
+++ 0138. Copy List With Random Pointer.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  2 09:52:47 2019
-
-@author: alberthsu
-"""
 
 """
+138. Copy List with Random Pointer
 A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
 Return a deep copy of the list.
 """
