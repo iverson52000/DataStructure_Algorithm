62496077882775af427a685d4679854ab4fb1bf0
Albert Hsu <iverson52000@gmail.com>
2020-05-29 09:25:22 -0700

    20200529

--- 0207. Course Schedule.py
+++ 0207. Course Schedule.py
@@ -5,21 +5,24 @@ Some courses may have prerequisites, for example to take course 0 you have to fi
 Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
 """
 
-#bfs
+# bfs
+
 
 class Solution:
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         g = collections.defaultdict(set)
-        indegree = {i:0 for i in range(numCourses)}
-        
+        indegree = {i: 0 for i in range(numCourses)}
+
         for edge in prerequisites:
-            out = edge[1]; inn = edge[0]
+            out = edge[1]
+            inn = edge[0]
             if inn not in g[out]:
                 g[out].add(inn)
                 indegree[inn] += 1
-        
+
         return self.bfs(g, indegree, numCourses)
-    
+
     def bfs(self, g, indegree, numCourses) -> bool:
         q = collections.deque()
         visited = set()
@@ -31,30 +34,36 @@ class Solution:
             visited.add(out)
             for inn in g[out]:
                 indegree[inn] -= 1
-                if indegree[inn] == 0: q.append(inn)
+                if indegree[inn] == 0:
+                    q.append(inn)
         return len(visited) == numCourses
 
-#dfs
+# dfs
+
 
 class Solution:
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         g = collections.defaultdict(set)
         visited = [0 for i in range(numCourses)]
-        
+
         for edge in prerequisites:
-            out = edge[1]; inn = edge[0]
+            out = edge[1]
+            inn = edge[0]
             g[out].add(inn)
         for i in range(numCourses):
             if not self.dfs(g, visited, i):
                 return False
         return True
-    
+
     def dfs(self, graph, visited, i) -> bool:
-        if visited[i] == -1: return False   #has been visited and has ring
-        if visited[i] == 1: return True     #has been visited but no ring
-        visited[i] = -1 	#visiting
+        if visited[i] == -1:
+            return False  # has been visited and has ring
+        if visited[i] == 1:
+            return True  # has been visited but no ring
+        visited[i] = -1  # visiting
         for j in graph[i]:
             if not self.dfs(graph, visited, j):
                 return False
-        visited[i] = 1      #done visited all neighbors
+        visited[i] = 1  # done visiting all neighbors
         return True
