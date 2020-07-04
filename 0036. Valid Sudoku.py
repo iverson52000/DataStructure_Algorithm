3723286231c6216b40739c3d07f2f23069ed2b81
Albert Hsu <iverson52000@gmail.com>
2020-07-04 10:50:37 -0700

    20200704

--- 0036. Valid Sudoku.py
+++ 0036. Valid Sudoku.py
@@ -1,26 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 16 10:36:06 2019
-
-@author: alberthsu
-"""
-
 """
 36. Valid Sudoku
 Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 """
 
+#hashset
+
+
 class Solution:
+
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        if not board: return False
+        if not board:
+            return False
         flag = set()
-        for r in range(board):
-            for c in range(board[0]):
+        for r in range(len(board)):
+            for c in range(len(board[0])):
                 if board[r][c] != '.':
                     num = board[r][c]
-                    if (r, num) in flag or (num, c) in flag or (r//3, c//3, num) in flag: return False
+                    if (r, num) in flag or (num, c) in flag or (r // 3, c // 3, num) in flag:
+                        return False
                     flag.add((r, num))
                     flag.add((num, c))
-                    flag.add((r//3, c//3, num))
+                    flag.add((r // 3, c // 3, num))
         return True
