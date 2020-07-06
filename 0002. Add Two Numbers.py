846daa267db4b68e4ae87d05be929a36e8d4e2db
Albert Hsu <iverson52000@gmail.com>
2020-07-06 09:19:06 -0700

    20200706

--- 0002. Add Two Numbers.py
+++ 0002. Add Two Numbers.py
@@ -4,10 +4,15 @@ The digits are stored in reverse order and each of their nodes contain a single 
 Add the two numbers and return it as a linked list.
 """
 
+# iteration
+
+
 class Solution:
+
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        if not l1 or not l2: return
-        carry = 0 
+        if not l1 or not l2:
+            return
+        carry = 0
         pre = l3 = ListNode(-1)
         while l1 or l2 or carry != 0:
             v1 = v2 = 0
@@ -17,7 +22,7 @@ class Solution:
             if l2:
                 v2 = l2.val
                 l2 = l2.next
-            carry, v3 = divmod(v1+v2+carry, 10)
+            carry, v3 = divmod(v1 + v2 + carry, 10)
             l3.next = ListNode(v3)
             l3 = l3.next
         return pre.next
