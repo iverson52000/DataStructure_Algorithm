2c298a8af9e080a0dd21fff5be7c5ba8d6dc95a8
Albert Hsu <iverson52000@gmail.com>
2020-07-13 09:31:48 -0700

    20200713

--- 0100. Same Tree.py
+++ 0100. Same Tree.py
@@ -4,7 +4,7 @@ Given two binary trees, write a function to check if they are the same or not.
 Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
 """
 
-#recursion
+# recursion
 
 # Definition for a binary tree node.
 # class TreeNode:
@@ -13,30 +13,38 @@ Two binary trees are considered the same if they are structurally identical and 
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
+
     def isSameTree(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
         """
-        if not p and not q: return True
-        if (not p and q) or (p and not q) or p.val != q.val: return False
+        if not p and not q:
+            return True
+        if (not p and q) or (p and not q) or p.val != q.val:
+            return False
         if p.val == q.val:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-#iteration
+# iteration
+
 
 class Solution:
+
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         stack = [(p, q)]
         while stack:
             node1, node2 = stack.pop()
-            if not node1 and not node2: continue
-            elif not node1 or not node2: return False
+            if not node1 and not node2:
+                continue
+            elif not node1 or not node2:
+                return False
             else:
-                if node1.val != node2.val: return False
+                if node1.val != node2.val:
+                    return False
                 stack.append((node1.left, node2.left))
                 stack.append((node1.right, node2.right))
-        return True 
-
+        return True
