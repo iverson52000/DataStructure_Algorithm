bcc85f1b2bc4a61d205a81972e3096ef997a4cad
Albert Hsu <iverson52000@gmail.com>
2020-07-31 09:23:19 -0700

    Update O(1) soution

--- 0070. Climbing Stairs.py
+++ 0070. Climbing Stairs.py
@@ -5,12 +5,25 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 Note: Given n will be a positive integer.
 """
 
-#dp
+# dp. Space: O(n) 
+
 
 class Solution:
+
     def climbStairs(self, n: int) -> int:
-        dp = [0]*(n+1)
+        dp = [0] * (n + 1)
         dp[0] = dp[1] = 1
-        for i in range(2, n+1):
-            dp[i] = dp[i-1]+dp[i-2]
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
         return dp[n]
+
+# dp. Space: O(1) 
+
+
+class Solution:
+
+    def climbStairs(self, n: int) -> int:
+        dp1 = dp2 = 1
+        for i in range(n):
+            dp1, dp2 = dp2, dp1 + dp2
+        return dp1
