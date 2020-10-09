1880c37fb3d6fc35414e603730d82f4d89391149
Albert Hsu <iverson52000@gmail.com>
2020-10-09 09:27:34 -0700

    20201009

---
+++ 0449. Serialize and Deserialize BST.py
@@ -0,0 +1,53 @@
+# 449. Serialize and Deserialize BST
+# Serialization is converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+# Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You need to ensure that a binary search tree can be serialized to a string, and this string can be deserialized to the original tree structure.
+# The encoded string should be as compact as possible.
+
+#dfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root: TreeNode) -> str:
+        """Encodes a tree to a single string.
+        """
+        vals = []
+
+        def dfs(node):
+            if node:
+                vals.append(node.val)
+                dfs(node.left)
+                dfs(node.right)
+
+        dfs(root)
+
+        return ' '.join(map(str, vals))
+
+    def deserialize(self, data: str) -> TreeNode:
+        """Decodes your encoded data to tree.
+        """
+        vals = collections.deque(int(val) for val in data.split())
+
+        def dfs(min_val, max_val):
+            if vals and min_val < vals[0] < max_val:
+                val = vals.popleft()
+                node = TreeNode(val)
+                node.left = dfs(min_val, val)
+                node.right = dfs(val, max_val)
+                return node
+
+        return dfs(float('-inf'), float('inf'))
+
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
