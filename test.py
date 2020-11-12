44d914e212e1e523ddd9f4d87ef5915afbfe6e19
Albert Hsu <iverson52000@gmail.com>
2020-11-12 09:44:13 -0800

    20201112

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pre, cur = 0 , 0
+        pre, cur = 0, 0
         i = 0
-        n = len(nums)       
+        n = len(nums)
         while cur < n-1:
             while i <= pre:
-                cur = max(i+nums[i],cur)
-                i+=1
-            if pre == cur: return False            
+                cur = max(i+nums[i], cur)
+                i += 1
+            if pre == cur:
+                return False
             pre = cur
         return True if cur >= n-1 else False
 
 
 #20201111
-
