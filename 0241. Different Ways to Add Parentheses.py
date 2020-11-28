ef7213b7846924616cea2e492e0997dde1f9aa9e
Albert Hsu <iverson52000@gmail.com>
2020-11-28 09:52:50 -0800

    20201128

--- 0241. Different Ways to Add Parentheses.py
+++ 0241. Different Ways to Add Parentheses.py
@@ -5,12 +5,15 @@ possible ways to group numbers and operators. The valid operators are +, - and *
 
 #dfs. divide and conquer
 
+
 class Solution:
     def diffWaysToCompute(self, input: str) -> List[int]:
-        if input.isdigit(): return [int(input)]
+        if input.isdigit():
+            return [int(input)]
         res = []
         memo = []
-        if input in memo: return memo[input]
+        if input in memo:
+            return memo[input]
         for i in range(len(input)):
             if input[i] in '+-*':
                 left_input = self.diffWaysToCompute(input[:i])
@@ -20,10 +23,11 @@ class Solution:
                         res.append(self.calc(left, right, input[i]))
         memo[input] = res
         return res
-    
+
     def calc(self, left, right, op) -> int:
-        if op == '+': return left+right
-        if op == '-': return left-right
-        if op == '*': return left*right
-
-
+        if op == '+':
+            return left+right
+        if op == '-':
+            return left-right
+        if op == '*':
+            return left*right
