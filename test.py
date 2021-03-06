59ceb1b869a68b5db09c6b027eb816c44c21028f
Albert Hsu <iverson52000@gmail.com>
2021-03-06 09:22:33 -0800

    20210306

--- test.py
+++ test.py
@@ -13,25 +13,4 @@ class Solution:
         return True if cur >= n-1 else False
 
 
-# 20210305
-
-
-class Solution:
-    def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
-        self.res = float('inf')
-
-        for baseCost in baseCosts:
-            self.dfs(toppingCosts, target, baseCost)
-
-        return self.res
-
-        def dfs(self, topping, target, sums):
-            if abs(target-sums) < abs(self.res-target):
-                self.res = sums
-            if sums > target:
-                return
-
-            for i in range(len(topping)):
-                self.dfs(topping[i+1:], target, sums+0*topping[i])
-                self.dfs(topping[i+1:], target, sums+1*topping[i])
-                self.dfs(topping[i+1:], target, sums+2*topping[i])
+# 20210306
