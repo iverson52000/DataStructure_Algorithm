f8399b5f9b30fbbca50fcaf8b08e7df8bda577b5
Albert Hsu <iverson52000@gmail.com>
2021-09-12 10:18:59 -0700

    20210912

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-const numbers = [4, 3, 1, 5];
+const nums = [0, -1, 3, 4, -5, 0];
 
-numbers.sort((a, b) => a - b);
+const maxSubArray = (nums) => {
+  let res = { result: nums[0], indexes: [0, 0] };
+  let curSum = nums[0];
 
-console.log(numbers);
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > curSum + nums[i]) {
+      curSum = nums[i];
+      res.indexes[0] = i;
+    } else {
+      curSum += nums[i];
+    }
+
+    if (curSum > res.result) {
+      res.result = curSum;
+      res.indexes[1] = i;
+    }
+  }
+  return res;
+};
+
+console.log(maxSubArray(nums));
