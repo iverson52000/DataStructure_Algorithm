fa57f4b0727adbe7ebb9c9bf08da1ed37c5ad931
Albert Hsu <iverson52000@gmail.com>
2021-10-22 09:41:53 -0700

    20211021

--- test.py
+++ test.py
@@ -3,15 +3,4 @@ import time
 import queue
 
 
-def fn(nums):
-    has_pos = False
-    has_neg = False
-    for num in nums:
-        has_pos = num > 0
-        has_neg = num < 0
-    return (has_pos, has_neg)
-
-
-print(fn([0, 1, 2]))
-
 # 20211021
