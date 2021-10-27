acb5d6e503eccc2bee0a20dba8ab985cac09597b
Albert Hsu <iverson52000@gmail.com>
2021-10-27 13:00:25 -0700

    20211027

--- test.py
+++ test.py
@@ -11,25 +11,18 @@ import queue
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minWindow(self, s1: str, s2: str) -> str:
-        m, n = len(s1), len(s2)
-        dp = [[float('inf')]*(m+1) for i in range(n+1)]
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        cnts, res = Counter(changed), []
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                if s1[j-1] == s2[i-1]:
-                    if i == 1:
-                        dp[i][j] = 1
-                    else:
-                        dp[i][j] = dp[i-1][j-1]+1
-                else:
-                    dp[i][j] = dp[i][j-1]+1
+        for num in sorted(changed, key=lambda x: abs(x)):
+            if cnts[num] == 0:
+                continue
+            if cnts[2*num] == 0:
+                return []
+            res += [num]
+            if num == 0 and cnts[num] <= 1:
+                return []
+            cnts[num] -= 1
+            cnts[2*num] -= 1
 
-        res = min(dp[-1])
-
-        if res == float('inf'):
-            return ''
-
-        for i in range(1, m+1):
-            if dp[-1][i] == res:
-                return s1[i-res:i]
+        return res
