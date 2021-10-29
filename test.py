f1e2ccffaf8823092f8045afb79f97c000ad28d2
Albert Hsu <iverson52000@gmail.com>
2021-10-29 11:10:41 -0700

    20211029

--- test.py
+++ test.py
@@ -11,18 +11,29 @@ import queue
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findOriginalArray(self, changed: List[int]) -> List[int]:
-        cnts, res = Counter(changed), []
+    def minimumCost(self, n: int, connections: List[List[int]]) -> int:
+        def find(city) -> int:
+            if parents[city] != city:
+                parents[city] = find(parents[city])
 
-        for num in sorted(changed, key=lambda x: abs(x)):
-            if cnts[num] == 0:
-                continue
-            if cnts[2*num] == 0:
-                return []
-            res += [num]
-            if num == 0 and cnts[num] <= 1:
-                return []
-            cnts[num] -= 1
-            cnts[2*num] -= 1
+            return parents[city]
 
-        return res
+        def union(c1, c2) -> bool:
+            root1, root2 = find(c1), find(c2)
+            if root1 == root2:
+                return False
+            parents[root2] = root1
+
+            return True
+
+        parents = {city: city for city in range(1, n+1)}
+        connections.sort(key=lambda x: x[2])
+        total = 0
+
+        for city1, city2, cost in connections:
+            if union(city1, city2):
+                total += cost
+
+        root = find(n)
+
+        return total if all(root == find(city) for city in range(1, n+1)) else -1
