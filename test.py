e8b1fc0155edcc0e5b7dc60099ee511206bfbf24
Albert Hsu <iverson52000@gmail.com>
2021-11-04 09:14:12 -0700

    20211004

--- test.py
+++ test.py
@@ -4,10 +4,12 @@ import queue
 from collections import *
 
 
-card1 = tuple([1, 2, 3, 1])
-card2 = tuple([2, 3, 1, 2])
-card3 = tuple([3, 1, 2, 3])
-
-print(card1 != card2 != card3)
-
-# test
+s = set(arr)
+  res = 2
+   for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            a, b, l = arr[i], arr[j], 2
+            while a + b in s:
+                a, b, l = b, a + b, l + 1
+            res = max(res, l)
+    return res if res > 2 else 0
