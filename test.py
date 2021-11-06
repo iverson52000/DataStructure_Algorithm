31f824fd028ce19987bc232a580d72561c27cb3a
Albert Hsu <iverson52000@gmail.com>
2021-11-06 13:46:47 -0700

    20211106

--- test.py
+++ test.py
@@ -1,15 +1,39 @@
+import collections
 import threading
 import time
 import queue
 from collections import *
 
 
-s = set(arr)
-  res = 2
-   for i in range(len(arr)):
-        for j in range(i + 1, len(arr)):
-            a, b, l = arr[i], arr[j], 2
-            while a + b in s:
-                a, b, l = b, a + b, l + 1
-            res = max(res, l)
-    return res if res > 2 else 0
+graph = collections.defaultdict(list)
+   for u, v in roads:
+        graph[u].append(v)
+        graph[v].append(u)
+
+    # init variables
+    m = len(targetPath)
+    dp = [[m] * n for _ in range(m)]
+    prev = [[0] * n for _ in range(m)]
+
+    # populate dp
+    for v in range(n):
+        dp[0][v] = (names[v] != targetPath[0])
+    for i in range(1, m):
+        for v in range(n):
+            for u in graph[v]:
+                if dp[i-1][u] < dp[i][v]:
+                    dp[i][v] = dp[i-1][u]
+                    prev[i][v] = u
+            dp[i][v] += (names[v] != targetPath[i])
+
+    # re-construct path
+    path, min_dist = [0], m
+    for v in range(n):
+        if dp[-1][v] < min_dist:
+            min_dist = dp[-1][v]
+            path[0] = v
+    for i in range(m - 1, 0, -1):
+        u = prev[i][path[-1]]
+        path.append(u)
+
+    return path[::-1]
