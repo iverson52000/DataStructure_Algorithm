5dcda2c8cf6574993b133324fb703863b73b1bb0
Albert Hsu <iverson52000@gmail.com>
2021-11-20 13:45:48 -0800

    airtable

--- test.py
+++ test.py
@@ -6,26 +6,39 @@ from collections import *
 from typing import List
 import random
 
+# input = "/home/airtable"
+# output = [
+#     [".bashrc", "Backups/2017_bashrc"],
+#     ["Photos/Vacation/DSC1234.JPG", "profile.jpeg", ".trash/lej2dp28/87msnlgyr"],
+# ]
 
 # APIs
 # list_folder("/home/airtable") returns ["Backups", ".bashrc", "Photos", "profile.jpeg", ...]
 # is_folder("/home/airtable/Backups") returns true
 # is_folder("/home/airtable/.bashrc") returns false
 
-def list_folder():
+
+def list_folder(path: str) -> List[str]:
+    """Return file or folder name only"""
     pass
 
 
-def is_folder():
+def is_folder(path: str) -> bool:
+    """Return true if is folder"""
     pass
 
 
-def getContent():
-    pass
+def getContent(path: str) -> str:
+    with open(path) as file:
+        contents = file.readlines()
+
+        return ''.join(contents)
+
+
+print(getContent("test.js"))
+
 
 # bfs
-
-
 def find_dupes(root_path: str) -> List[List[str]]:
     if not root_path:
         return []
@@ -35,13 +48,13 @@ def find_dupes(root_path: str) -> List[List[str]]:
 
     while q:
         rootPath = q.popleft()
-        paths = [rootPath+'/'+item for item in list_folder(rootPath)]
+        paths = [rootPath+'/'+name for name in list_folder(rootPath)]
 
         for path in paths:
             if not is_folder(path):
-                fileName = path.split('/')[-1]
+                # fileName = path.split('/')[-1]
                 # get content?
-                content = getContent(fileName)
+                content = getContent(path)
                 m[content].append(path)
 
             nextPath = rootPath+'/'+path
@@ -49,9 +62,8 @@ def find_dupes(root_path: str) -> List[List[str]]:
 
     return [val for val in m.values() if len(val) > 1]
 
+
 # dfs
-
-
 def find_dupes(root_path: str) -> List[List[str]]:
     if not root_path:
         return []
@@ -63,13 +75,13 @@ def find_dupes(root_path: str) -> List[List[str]]:
             return
 
         if not is_folder(rootPath):
-            fileName = rootPath.split('/')[-1]
+            # fileName = rootPath.split('/')[-1]
             # get content?
-            content = getContent(fileName)
+            content = getContent(rootPath)
             m[content].append(rootPath)
             return
 
-        paths = [rootPath+'/'+item for item in list_folder(rootPath)]
+        paths = [rootPath+'/'+name for name in list_folder(rootPath)]
 
         for path in paths:
             nextPath = rootPath+'/'+path
