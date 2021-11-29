5e01c055e7871fedb08955d284d628c50bf48d28
Albert Hsu <iverson52000@gmail.com>
2021-11-28 19:42:52 -0800

    20211128

--- test.py
+++ test.py
@@ -1,18 +1,26 @@
 import collections
 import threading
 import time
-import q
+from heapq import *
 from collections import *
 from typing import List
 import random
 
 
 class Solution:
-    def reorderSpaces(self, text: str) -> str:
-        words = text.split()  # split(sep=None) will discard empty strings.
-        n_words = len(words)
-        n_space = text.count(' ')
-        gap = 0 if n_words == 1 else n_space // (n_words - 1)
-        n_trailing = n_space - gap * (n_words - 1)  # credit to @madno
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        heap = []
 
-        return (' ' * gap).join(words) + ' ' * n_trailing
+        for n, start, end in trips:
+            heappush(heap, (start, n))
+            heappush(heap, (end, -n))
+
+        passenger = 0
+
+        while heap:
+            _, n = heappop(heap)
+            passenger += n
+            if passenger > capacity:
+                return False
+
+        return True
