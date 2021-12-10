09507953d13db545dc6074e5eff5ac87a84d6fcd
Albert Hsu <iverson52000@gmail.com>
2021-12-09 19:01:46 -0800

    20211209

--- test.py
+++ test.py
@@ -7,26 +7,30 @@ from typing import *
 import random
 
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def dfs(node) -> Tuple[int, TreeNode]:
-            if not node:
-                return 0, None
-            leftRes, rightRes = dfs(node.left), dfs(node.right)
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
 
-            if leftRes[0] > rightRes[0]:
-                return leftRes[0]+1, leftRes[1]
-            elif leftRes[0] < rightRes[0]:
-                return rightRes[0]+1, rightRes[1]
-            else:
-                return rightRes[0]+1, node
+        if not isConnected:
+            return 0
 
-        return dfs(root)[1]
+        visited = set()
+        res = 0
+
+        for i in range(len(isConnected)):
+            if i not in visited:
+                q = collections.deque([i])
+                while q:
+                    p = q.popleft()
+                    if p not in visited:
+                        visited.add(p)
+                        q += [k for k,
+                              adj in enumerate(isConnected[p]) if adj and (k not in visited)]
+                res += 1
+
+        return res
 
 # 20211209
