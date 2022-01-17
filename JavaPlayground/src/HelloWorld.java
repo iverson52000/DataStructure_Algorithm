8d1ae0b4cda7f4d8fc62377a25d789a7060af3ef
Albert Hsu <iverson52000@gmail.com>
2022-01-17 09:47:24 -0800

    JavaPlayground in vscode

--- JavaPlayground/src/HelloWorld.java
+++ JavaPlayground/src/HelloWorld.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class HelloWorld {
-    public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+    public static void main(String[] args) {
+        System.out.println("Hello java 17!");
+
+        int[][] input = { { 1, 3 }, { -2, 2 } };
+        int[][] res = kClosest(input, 2);
+
+        System.out.println(Arrays.deepToString(res));
+
+    }
+
+    public static int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
 
         for (int[] point : points) {
             pq.offer(point);
@@ -18,5 +30,6 @@ public class HelloWorld {
 
         System.out.println();
         return res;
+
     }
 }
