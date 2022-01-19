1dd605c3b2429ab9e57326b5d604ca89fe633e2e
Albert Hsu <iverson52000@gmail.com>
2022-01-19 14:58:56 -0800

    20220119

---
+++ JavaPlayground/src/Car.java
@@ -0,0 +1,9 @@
+public class Car extends Machine {
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        // super.start();
+        System.out.println("Car started");
+    }
+
+}
