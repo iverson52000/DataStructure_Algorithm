f61e8ce047fd82fc4a9743b17f91136e802a2f3c
Albert Hsu <iverson52000@gmail.com>
2022-01-21 13:36:18 -0800

    20220121

--- JavaPlayground/src/HelloWorld.java
+++ JavaPlayground/src/HelloWorld.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.PriorityQueue;
-
 class Frog {
     private int age;
     private String name;
@@ -28,37 +25,18 @@ class Frog {
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello java 17!");
 
-        int[][] input = { { 1, 3 }, { -2, 2 } };
-        int[][] res = kClosest(input, 1);
-
+        // int[][] input = { { 1, 3 }, { -2, 2 } };
+        // int[][] res = kClosest(input, 1);
         // System.out.println(Arrays.deepToString(res));
 
         Frog frog1 = new Frog();
-        frog1.showInfo();
+        Frog.showInfo();
 
         StringBuilder sb = new StringBuilder("");
+        Machine mech1 = new Machine();
+        Car car1 = new Car();
+        car1.start();
     }
 
-    public static int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-                (p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
-
-        for (int[] point : points) {
-            pq.offer(point);
-            if (pq.size() > k) {
-                pq.poll();
-            }
-        }
-
-        int[][] res = new int[k][2];
-
-        for (int i = 0; i < k; i++) {
-            res[i] = pq.poll();
-        }
-
-        return res;
-
-    }
 }
