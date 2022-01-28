2114bb6355f4d5066213faaa543ccaa7a9a1900c
Albert Hsu <iverson52000@gmail.com>
2022-01-27 16:09:48 -0800

    Kotlin class

--- KotlinPlayground/src/main/kotlin/Main.kt
+++ KotlinPlayground/src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val nums = arrayOf(1, 2, 3)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    nums.forEach { num -> println(num) }
 }
