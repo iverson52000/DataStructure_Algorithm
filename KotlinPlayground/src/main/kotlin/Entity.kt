9a6aebab15c47e876f4e35e9d04a57ea0a3eb869
Albert Hsu <iverson52000@gmail.com>
2022-01-28 19:21:35 -0800

    Kotlin object

---
+++ KotlinPlayground/src/main/kotlin/Entity.kt
@@ -0,0 +1,14 @@
+object EntityFactory {
+    fun create() = Entity("id", "name")
+}
+
+class Entity(val id: String, val name: String) {
+    override fun toString(): String {
+        return "id: $id name: $name"
+    }
+}
+
+fun main() {
+    val entity = EntityFactory.create()
+    println(entity)
+}
