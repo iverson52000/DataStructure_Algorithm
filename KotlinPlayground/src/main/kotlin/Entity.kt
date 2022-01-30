28257a17a08ce2da24642fc4657a517cb9ac2f65
Albert Hsu <iverson52000@gmail.com>
2022-01-29 20:16:46 -0800

    Advanced funcion

--- KotlinPlayground/src/main/kotlin/Entity.kt
+++ KotlinPlayground/src/main/kotlin/Entity.kt
@@ -1,14 +1,31 @@
-object EntityFactory {
-    fun create() = Entity("id", "name")
-}
-
-class Entity(val id: String, val name: String) {
-    override fun toString(): String {
-        return "id: $id name: $name"
-    }
-}
-
-fun main() {
-    val entity = EntityFactory.create()
-    println(entity)
-}
+//enum class EntityType {
+//    EASY, MEDIUM, HARD;
+//
+//    fun getFormattedName() = name.lowercase().replaceFirstChar { it.uppercase() }
+//}
+//
+//object EntityFactory {
+//    fun create() = Entity("id", "name")
+//}
+//
+//sealed class Entity() {
+//    object Help {
+//        val name = "Help"
+//    }
+//
+//    data class Easy(val id: String, val name: String) : Entity()
+//    data class Medium(val id: String, val name: String) : Entity()
+//    data class Hard(val id: String, val name: String, val modifier: Float) : Entity()
+//}
+//
+//fun Entity.Medium.printInfo() {
+//    println("Medium class: $id")
+//}
+//
+//val Entity.Medium.info: String
+//    get() = "some info"
+//
+//fun main() {
+//    val entity = EntityFactory.create()
+//    println(entity)
+//}
