761b6f0972fd11e60aebb06eca364bb56981eed1
Albert Hsu <iverson52000@gmail.com>
2022-03-19 09:46:51 -0700

    Relocate files

--- JavaPlayground/src/Car.java
+++
@@ -1,9 +0,0 @@
-public class Car extends Machine {
-    @Override
-    public void start() {
-
-        // super.start();
-        System.out.println("Car started");
-    }
-
-}
--- JavaPlayground/src/Test1.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Test1 {
-    public void run() throws IOException {
-        throw new IOException();
-
-    }
-
-}
