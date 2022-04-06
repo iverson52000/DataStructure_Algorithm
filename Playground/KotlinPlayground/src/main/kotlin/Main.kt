38e9289be8955881246d494f52aaf7b2bada38fe
Albert Hsu <iverson52000@gmail.com>
2022-04-05 20:30:24 -0700

    Kotlin test

--- Playground/KotlinPlayground/src/main/kotlin/Main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/Main.kt
@@ -1,48 +1,28 @@
-import java.util.UUID
+class Animal(var type: String, var size: String) {
+    // custom setter and getter
+    var nickName: String? = null
+        set(value) {
+            field = value
+            println("the new nick name is $value")
+        }
+        get() {
+            println("the returned value is $field")
+            return field
+        }
 
-enum class EntityType {
-    HELP, EASY, MEDIUM, HARD;
-
-    fun getFormattedName() = name.lowercase().replaceFirstChar { it.uppercase() }
-}
-
-sealed class Entity {
-    object Help : Entity() {
-        const val name = "Help"
+    fun printInfo() {
+        println("Hi!")
+        val nickNameToPrint = nickName ?: "no nickname"
+        printInfo2()
     }
 
-    data class Easy(val id: String, val name: String) : Entity()
-    data class Medium(val id: String, val name: String) : Entity()
-    data class Hard(val id: String, val name: String, val multiplier: Float) : Entity()
-}
-
-object EntityFactory {
-    fun create(type: EntityType): Entity {
-        val id = UUID.randomUUID().toString()
-        val name = when (type) {
-            EntityType.EASY -> type.getFormattedName()
-            EntityType.MEDIUM -> type.getFormattedName()
-            EntityType.HARD -> type.getFormattedName()
-            EntityType.HELP -> type.getFormattedName()
-        }
-        return when (type) {
-            EntityType.EASY -> Entity.Easy(id, name)
-            EntityType.MEDIUM -> Entity.Medium(id, name)
-            EntityType.HARD -> Entity.Hard(id, name, 2f)
-            EntityType.HELP -> Entity.Help
-        }
+    private fun printInfo2() {
+        println("Hi from info 2!")
+        val nickNameToPrint = nickName ?: "no nickname"
     }
 }
 
 fun main() {
-    val easyEntity = EntityFactory.create(EntityType.EASY)
-    val message = when (easyEntity) {
-        Entity.Help -> "help class"
-        is Entity.Easy -> "easy class"
-        is Entity.Medium -> "medium class"
-        is Entity.Hard -> "hard class"
-    }
-
-    println(message)
-    println(easyEntity)
+    val animal = Animal("a", "b")
+    animal.printInfo()
 }
