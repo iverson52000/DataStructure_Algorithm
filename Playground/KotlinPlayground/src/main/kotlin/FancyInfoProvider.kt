2cdc42a0c532d9059a5b36d3642f14e2f9ae729b
Albert Hsu <iverson52000@gmail.com>
2022-04-16 09:57:07 -0700

    Kotlin in Gradle

--- Playground/KotlinPlayground/src/main/kotlin/FancyInfoProvider.kt
+++
@@ -1,8 +0,0 @@
-class FancyInfoProvider : BasicInfoProvider() {
-    override val providerInfo: String
-        get() = super.providerInfo
-
-    override fun printInto(person: Person) {
-        super.printInto(person)
-    }
-}
