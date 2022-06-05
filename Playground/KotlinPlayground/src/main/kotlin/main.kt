8b2a285bb95bde09db643a0535a489cd8d8e07b0
Albert Hsu <iverson52000@gmail.com>
2022-06-05 10:30:58 -0700

    Install kotlinx with gradle

--- Playground/KotlinPlayground/src/main/kotlin/main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/main.kt
@@ -1,11 +1,19 @@
-class Intent {
-    var a = 10
-}
+import kotlinx.coroutines.*
 
-fun main() {
-    val intent = Intent().apply {
-        this.a = 5
+fun main() = runBlocking {    // Creates a blocking coroutine that executes in current thread (main)
+
+    println("Main program starts: ${Thread.currentThread().name}")  // main thread
+
+    val deferredJob: Deferred<Int> = async {   // Thread: main
+        println("Fake work starts: ${Thread.currentThread().name}")     // Thread: main
+        delay(1000)   // Coroutine is suspended but Thread: main is free (not blocked)
+        println("Fake work finished: ${Thread.currentThread().name}") // Thread: main
+        15  // Will be returned
     }
 
-    println(intent.a)
+    val num: Int =
+        deferredJob.await()  // main thread: wait for coroutine to finish and return data. Can call deferredJob.join()
+
+    println("Main program ends: ${Thread.currentThread().name}")    // main thread
+    println(num)
 }
