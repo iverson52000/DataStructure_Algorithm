448f1b5307e375ef0aa3946a43c030c2481dcee0
Albert Hsu <iverson52000@gmail.com>
2022-06-13 20:34:48 -0700

    20220613

--- Playground/KotlinPlayground/src/main/kotlin/main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/main.kt
@@ -4,16 +4,16 @@ fun main() = runBlocking {    // Creates a blocking coroutine that executes in c
 
     println("Main program starts: ${Thread.currentThread().name}")  // main thread
 
-    val deferredJob: Deferred<Int> = async {   // Thread: main
-        println("Fake work starts: ${Thread.currentThread().name}")     // Thread: main
-        delay(1000)   // Coroutine is suspended but Thread: main is free (not blocked)
-        println("Fake work finished: ${Thread.currentThread().name}") // Thread: main
-        15  // Will be returned
+    val job: Job = launch {     // Thread main: Creates a non-blocking coroutine
+        for (i in 0..500) {
+            print("$i.")
+            yield()     // or use delay() or any other suspending function as per your need.
+        }
     }
 
-    val num: Int =
-        deferredJob.await()  // main thread: wait for coroutine to finish and return data. Can call deferredJob.join()
+    delay(5)  // Let's print a few values before we cancel
+    job.cancelAndJoin()
 
-    println("Main program ends: ${Thread.currentThread().name}")    // main thread
-    println(num)
+    println("\nMain program ends: ${Thread.currentThread().name}")    // main thread
 }
+
