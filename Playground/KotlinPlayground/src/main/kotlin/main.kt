a6a0d62d89d9372a62422095ddfcbde581af9943
Albert Hsu <iverson52000@gmail.com>
2022-06-24 20:07:10 -0700

    20220624

--- Playground/KotlinPlayground/src/main/kotlin/main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/main.kt
@@ -1,26 +1,13 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
+class Person {
+    val height = 100
+}
+
 fun main() = runBlocking<Unit> {
 
-    val flow = flow {
-        delay(250L)
-        emit("Appetizer")
-        delay(1000L)
-        emit("Main dish")
-        delay(100L)
-        emit("Dessert")
-    }
-    
-    launch {
-        flow.onEach {
-            println("FLOW: $it is delivered")
-        }.buffer().collect {
-            println("FLOW: Now eating $it")
-            delay(1500L)
-            println("FLOW: Finished eating $it")
-        }
-    }
+    val personHeight = Person.height
 
 }
 
