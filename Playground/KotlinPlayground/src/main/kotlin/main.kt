acabca3d2c0c4c610186b1e6c156161990f8b3c9
Albert Hsu <iverson52000@gmail.com>
2022-06-26 10:19:08 -0700

    20220626

--- Playground/KotlinPlayground/src/main/kotlin/main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/main.kt
@@ -1,14 +1,21 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
-class Person {
-    val height = 100
+class CustomTriple<A : Any, B : Any, C : Any>(
+    val first: A,
+    val second: B,
+    val third: C
+) {
+    fun printType() {
+        println("The type of first ${first::class}")
+        println("The type of second ${second::class}")
+        println("The type of third ${third::class}")
+    }
 }
 
-fun main() = runBlocking<Unit> {
-
-    val personHeight = Person.height
-
+fun main() {
+    val customTriple = CustomTriple(1, "test", 5.0)
+    customTriple.printType()
 }
 
 
