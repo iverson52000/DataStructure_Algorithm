40e7e8557d9baf0f6d0d9dd41a26c19eb1a76508
Albert Hsu <iverson52000@gmail.com>
2022-07-12 20:33:02 -0700

    20220712

--- Playground/KotlinPlayground/src/main/kotlin/main.kt
+++ Playground/KotlinPlayground/src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import org.apache.commons.math3.distribution.LaplaceDistribution
+import kotlin.math.min
+import kotlin.random.Random
 
 class CustomTriple<A : Any, B : Any, C : Any>(
     val first: A,
@@ -13,9 +16,21 @@ class CustomTriple<A : Any, B : Any, C : Any>(
     }
 }
 
+const val testVal = "*/"
+fun displayBorder(character: String = testVal, length: Int = 15) {
+    for (i in 1..length) {
+        print(character)
+    }
+}
+
 fun main() {
-    val customTriple = CustomTriple(1, "test", 5.0)
+    val customTriple = CustomTriple(1, 2, 3)
     customTriple.printType()
+    displayBorder()
+    val arr = listOf(1, 2, 3)
+    val arrSize = min(arr.size, 10)
+    println(arr.subList(0, arrSize))
+
 }
 
 
