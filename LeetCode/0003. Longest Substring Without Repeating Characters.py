eb0ea093c3d0f388a5725bdd71b659dbd200e1de
Albert Hsu <iverson52000@gmail.com>
2022-08-03 17:16:19 -0700

    20220803

--- LeetCode/0003. Longest Substring Without Repeating Characters.py
+++ LeetCode/0003. Longest Substring Without Repeating Characters.py
@@ -3,16 +3,18 @@
 Given a string, find the length of the longest substring without repeating characters.
 """
 
-#Sliding window
+# Sliding window
+
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         res = 0
         left = -1
         n = len(s)
-        m = {}       
+        m = {}
         for i in range(n):
-            if s[i] in m and m[s[i]] > left: left = m[s[i]]                
+            if s[i] in m and m[s[i]] > left:
+                left = m[s[i]]
             m[s[i]] = i
             res = max(res, i-left)
-        return res 
+        return res
