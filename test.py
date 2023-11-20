9d996029dbcd4bef6cae26b4dadbc3665e44c9e4
Albert Hsu <iverson52000@gmail.com>
2023-11-20 20:33:40 +0800

    Script to convert spaced string to camel case string

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-# testing
+unit_test = "computeImpressionCappedHistogram returns expected result with small maxThreshold"
+unit_test_list = unit_test.split()
+
+for i in range(1, len(unit_test_list)):
+    unit_test_list[i] = unit_test_list[i].capitalize()
+
+unit_test_final = ''.join(unit_test_list)
+print(unit_test_final)
 
 # 20231120
